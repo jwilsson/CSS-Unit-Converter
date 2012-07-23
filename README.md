f97f821c2facfc4b1a1eae18746d31a2c1ba3a8f
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2012-07-23 21:11:13 +0200

    heavy optimization of the js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # CSS Unit Converter
-Use the CSS Unit Converter to convert values between almost every CSS &lt;length&gt; unit.
+Use the CSS Unit Converter to convert values between almost every CSS [&lt;length&gt;](https://developer.mozilla.org/en/CSS/length) unit.
 
 Check it out in action at http://jonathanwilsson.com/unit-converter/.
--- inc/unit-converter.js
+++ inc/unit-converter.js
@@ -1,308 +1,96 @@
-// CSS Unit converter 1.4 Copyright 2011-2012 Jonathan Wilsson
-(function () {
-	var UnitConverter = {
-		fromCM: function(value, to, base, dpi) {
-			var result;
+// CSS Unit converter 1.5 Copyright 2011-2012 Jonathan Wilsson
+function roundNumber (number, decimals) {
+	return Math.round(number * Math.pow(10, decimals)) / Math.pow(10, decimals);
+}
 
-			switch (to) {
-				case "em":
-					result = value / 0.42175176;
-					break;
-				case "in":
-					result = value * 0.39;
-					break;
-				case "mm":
-					result = value * 10;
-					break;
-				case "pc":
-					result = value / 0.42175176;
-					break;
-				case "pt":
-					result = value * 28.3464566929;
-					break;
-				case "%":
-					result = value / base * 100 / 2.54 * dpi;
-					break;
-				case "px":
-					result = value / 2.54 * dpi;
-					break;
-				case "rem":
-					result = value / 0.42175176;
-					break;
-			}
-
-			return result;
-		},
-		fromEM: function(value, to, base) {
-			var result;
-
-			switch (to) {
-				case "cm":
-					result = value * 0.42175176;
-					break;
-				case "in":
-					result = value * 0.166044;
-					break;
-				case "mm":
-					result = value / 0.237106301584;
-					break;
-				case "pc":
-					result = value;
-					break;
-				case "pt":
-					result = value * 11.955168;
-					break;
-				case "%":
-					result = value * 100;
-					break;
-				case "px":
-					result = value * base;
-					break;
-				case "rem":
-					result = value;
-					break;
-			}
-
-			return result;
-		},
-		fromIN: function(value, to, base, dpi) {
-			var result;
-
-			switch (to) {
-				case "cm":
-					result = value * 2.54;
-					break;
-				case "em":
-					result = value / 0.166044;
-					break;
-				case "mm":
-					result = value * 2.54 * 10;
-					break;
-				case "pc":
-					result = value / 0.166044;
-					break;
-				case "pt":
-					result = value / 0.014842519685;
-					break;
-				case "%":
-					result = value / base * 100 * dpi;
-					break;
-				case "px":
-					result = value * dpi;
-					break;
-				case "rem":
-					result = value / 0.166044;
-					break;
-			}
-
-			return result;
-		},
-		fromMM: function(value, to, base, dpi) {
-			var result;
-
-			switch (to) {
-				case "cm":
-					result = value / 10;
-					break;
-				case "em":
-					result = value * 0.237106301584;
-					break;
-				case "in":
-					result = value * 0.39 / 10;
-					break;
-				case "pc":
-					result = value / 4.42175176;
-					break;
-				case "pt":
-					result = value / 0.352777777778;
-					break;
-				case "%":
-					result = value / base * 100 / 2.54 * dpi / 10;
-					break;
-				case "px":
-					result = value / 2.54 * dpi / 10;
-					break;
-				case "rem":
-					result = value * 0.237106301584;
-					break;
-			}
-
-			return result;
-		},
-		fromPC: function(value, to, base) {
-			var result;
-
-			switch (to) {
-				case "cm":
-					result = value * 0.42175176;
-					break;
-				case "em":
-					result = value;
-					break;
-				case "in":
-					result = value * 0.166044;
-					break;
-				case "mm":
-					result = value * 4.42175176;
-					break;
-				case "pt":
-					result = value / 0.0836458341698;
-					break;
-				case "%":
-					result = value * 100;
-					break;
-				case "px":
-					result = value * base;
-					break;
-				case "rem":
-					result = value;
-					break;
-			}
-
-			return result;
-		},
-		fromPT: function(value, to, base) {
-			var result;
-
-			switch (to) {
-				case "cm":
-					result = value / 28.3464566929;
-					break;
-				case "em":
-					result = value / 11.955168;
-					break;
-				case "in":
-					result = value * 0.014842519685;
-					break;
-				case "mm":
-					result = value * 0.352777777778;
-					break;
-				case "pc":
-					result = value * 0.0836458341698;
-					break;
-				case "%":
-					result = value / (base - 4) * 100;
-					break;
-				case "px":
-					result = value * 96 / 72;
-					break;
-				case "rem":
-					result = value / 11.955168;
-					break;
-			}
-
-			return result;
-		},
-		fromPR: function(value, to, base, dpi) {
-			var result;
-
-			switch (to) {
-				case "cm":
-					result = value * base / 100 * 2.54 / dpi;
-					break;
-				case "em":
-					result = value / 100;
-					break;
-				case "in":
-					result = value * base / 100 / dpi;
-					break;
-				case "mm":
-					result = value * base / 100 * 2.54 / dpi * 10;
-					break;
-				case "pc":
-					result = value / 100;
-					break;
-				case "pt":
-					result = value * (base - 4) / 100;
-					break;
-				case "px":
-					result = value * base / 100;
-					break;
-				case "rem":
-					result = value / 100;
-					break;
-			}
-
-			return result;
-		},
-		fromPX: function(value, to, base, dpi) {
-			var result;
-
-			switch (to) {
-				case "cm":
-					result = value * 2.54 / dpi;
-					break;
-				case "em":
-					result = value / base;
-					break;
-				case "in":
-					result = value / dpi;
-					break;
-				case "mm":
-					result = value * 2.54 / dpi * 10;
-					break;
-				case "pc":
-					result = value / base;
-					break;
-				case "pt":
-					result = value * 72 / 96;
-					break;
-				case "%":
-					result = value / base * 100;
-					break;
-				case "rem":
-					result = value / base;
-					break;
-			}
-
-			return result;
-		},
-		fromREM: function(value, to, base) {
-			var result;
-
-			switch (to) {
-				case "cm":
-					result = value * 0.42175176;
-					break;
-				case "em":
-					result = value;
-					break;
-				case "in":
-					result = value * 0.166044;
-					break;
-				case "mm":
-					result = value / 0.237106301584;
-					break;
-				case "pc":
-					result = value;
-					break;
-				case "pt":
-					result = value * 11.955168;
-					break;
-				case "%":
-					result = value * 100;
-					break;
-				case "px":
-					result = value * base;
-					break;
-			}
-
-			return result;
-		}
+function convert (value, from, to, base, dpi, decimals) {
+	var units = from + "-" + to, result, formulas = {
+		"cm-em": value / 0.42175176,
+		"cm-in": value * 0.39,
+		"cm-mm": value * 10,
+		"cm-pc": value / 0.42175176,
+		"cm-pt": value * 28.3464566929,
+		"cm-%": value / base * 100 / 2.54 * dpi,
+		"cm-px": value / 2.54 * dpi,
+		"cm-rem": value / 0.42175176,
+	
+		"em-cm": value * 0.42175176,
+		"em-in": value * 0.166044,
+		"em-mm": value / 0.237106301584,
+		"em-pc": value,
+		"em-pt": value * 11.955168,
+		"em-%": value * 100,
+		"em-px": value * base,
+		"em-rem": value,
+	
+		"in-cm": value * 2.54,
+		"in-em": value / 0.166044,
+		"in-mm": value * 2.54 * 10,
+		"in-pc": value / 0.166044,
+		"in-pt": value / 0.014842519685,
+		"in-%": value / base * 100 * dpi,
+		"in-px": value * dpi,
+		"in-rem": value / 0.166044,
+	
+		"mm-cm": value / 10,
+		"mm-em": value * 0.237106301584,
+		"mm-in": value * 0.39 / 10,
+		"mm-pc": value / 4.42175176,
+		"mm-pt": value / 0.352777777778,
+		"mm-%": value / base * 100 / 2.54 * dpi / 10,
+		"mm-px": value / 2.54 * dpi / 10,
+		"mm-rem": value * 0.237106301584,
+	
+		"pc-cm": value * 0.42175176,
+		"pc-em": value,
+		"pc-in": value * 0.166044,
+		"pc-mm": value * 4.42175176,
+		"pc-pt": value / 0.0836458341698,
+		"pc-%": value * 100,
+		"pc-px": value * base,
+		"pc-rem": value,
+	
+		"pt-cm": value / 28.3464566929,
+		"pt-em": value / 11.955168,
+		"pt-in": value * 0.014842519685,
+		"pt-mm": value * 0.352777777778,
+		"pt-pc": value * 0.0836458341698,
+		"pt-%": value / (base - 4) * 100,
+		"pt-px": value * 96 / 72,
+		"pt-rem": value / 11.955168,
+	
+		"%-cm": value * base / 100 * 2.54 / dpi,
+		"%-em": value / 100,
+		"%-in": value * base / 100 / dpi,
+		"%-mm": value * base / 100 * 2.54 / dpi * 10,
+		"%-pc": value / 100,
+		"%-pt": value * (base - 4) / 100,
+		"%-px": value * base / 100,
+		"%-rem": value / 100,
+	
+		"px-cm": value * 2.54 / dpi,
+		"px-em": value / base,
+		"px-in": value / dpi,
+		"px-mm": value * 2.54 / dpi * 10,
+		"px-pc": value / base,
+		"px-pt": value * 72 / 96,
+		"px-%": value / base * 100,
+		"px-rem": value / base,
+	
+		"rem-cm": value * 0.42175176,
+		"rem-em": value,
+		"rem-in": value * 0.166044,
+		"rem-mm": value / 0.237106301584,
+		"rem-pc": value,
+		"rem-pt": value * 11.955168,
+		"rem-%": value * 100,
+		"rem-px": value * base
 	};
 
-	function roundNumber(number, decimals) {
-		return Math.round(number * Math.pow(10, decimals)) / Math.pow(10, decimals);
-	}
+	result = formulas[units] || false;
 
-	function init(value, from, to, base, dpi, decimals) {
-		var func = "from" + from.toUpperCase(), result = UnitConverter[func](value, to, base, dpi);
-
-		return (isNaN(result) ? "N/A" : roundNumber(result, decimals) + to);
-	}
-
-	window.UnitConverter = init;
-}());
+	return (isNaN(result) ? "N/A" : roundNumber(result, decimals) + to);
+}
 
 function run() {
 	document.getElementById("result").innerHTML = UnitConverter(
@@ -316,8 +104,9 @@ function run() {
 }
 
 window.addEventListener("DOMContentLoaded", function () {
-	// Listen for change on all <input> and <select>s
+	// Listen for change on all <input>s and <select>s
 	var elems = document.querySelectorAll("input, select"), i, len = elems.length;
+
 	for (i = 0; i < len; i++) {
 		elems[i].addEventListener("keyup", run, false);
 		elems[i].addEventListener("change", run, false);
