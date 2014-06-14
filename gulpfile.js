9dabdf8855bd0b7a529ddef45992838ee8bd76de
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2014-06-14 14:25:50 +0200

    Add JSHint

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
 var minify = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var rev = require('gulp-rev-append');
@@ -6,10 +7,16 @@ var uglify = require('gulp-uglify');
 
 var path = 'assets/';
 
+gulp.task('jshint', function () {
+    return gulp.src('./assets/script.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
 gulp.task('minify', function () {
     var name = 'style';
 
-    gulp.src(path + name + '.css')
+    return gulp.src(path + name + '.css')
         .pipe(minify())
         .pipe(rename(path + name + '.min.css'))
         .pipe(gulp.dest('.'));
@@ -18,16 +25,16 @@ gulp.task('minify', function () {
 gulp.task('uglify', function () {
     var name = 'script';
 
-    gulp.src(path + name + '.js')
+    return gulp.src(path + name + '.js')
         .pipe(uglify())
         .pipe(rename(path + name + '.min.js'))
         .pipe(gulp.dest('.'));
 });
 
 gulp.task('rev', function () {
-    gulp.src('./index.html')
-    .pipe(rev())
-    .pipe(gulp.dest('.'));
+    return gulp.src('./index.html')
+        .pipe(rev())
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task('default', ['minify', 'uglify', 'rev']);
+gulp.task('default', ['jshint', 'minify', 'uglify', 'rev']);
