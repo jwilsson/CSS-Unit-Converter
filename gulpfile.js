d62feed97a394c348ef9bd92390669c60e5c0673
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2014-06-14 09:49:53 +0200

    More dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+var changed = require('gulp-changed');
+var minify = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
 
