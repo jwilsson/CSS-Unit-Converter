fee28fc63cb84d9a62e984e332a83de61064dde8
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2015-06-15 13:45:22 +0200

    Use more ES2015 features

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-;(function () {
+;(function (window, document) {
     'use strict';
 
     var round = function (number, decimals) {
@@ -8,89 +8,91 @@
     var convert = function (value, from, to, base, dpi, decimals) {
         var units = from + '-' + to;
         var result;
-        var formulas = {
-                'cm-em': value / 0.42175176,
-                'cm-in': value * 0.39,
-                'cm-mm': value * 10,
-                'cm-pc': value / 0.42175176,
-                'cm-pt': value * 28.3464566929,
-                'cm-%': value / base * 100 / 2.54 * dpi,
-                'cm-px': value / 2.54 * dpi,
 
-                'em-cm': value * 0.42175176,
-                'em-in': value * 0.166044,
-                'em-mm': value / 0.237106301584,
-                'em-pc': value,
-                'em-pt': value * 11.955168,
-                'em-%': value * 100,
-                'em-px': value * base,
+        const FORMULAS = {
+            'cm-em': value / 0.42175176,
+            'cm-in': value * 0.39,
+            'cm-mm': value * 10,
+            'cm-pc': value / 0.42175176,
+            'cm-pt': value * 28.3464566929,
+            'cm-%': value / base * 100 / 2.54 * dpi,
+            'cm-px': value / 2.54 * dpi,
 
-                'in-cm': value * 2.54,
-                'in-em': value / 0.166044,
-                'in-mm': value * 2.54 * 10,
-                'in-pc': value / 0.166044,
-                'in-pt': value / 0.014842519685,
-                'in-%': value / base * 100 * dpi,
-                'in-px': value * dpi,
+            'em-cm': value * 0.42175176,
+            'em-in': value * 0.166044,
+            'em-mm': value / 0.237106301584,
+            'em-pc': value,
+            'em-pt': value * 11.955168,
+            'em-%': value * 100,
+            'em-px': value * base,
 
-                'mm-cm': value / 10,
-                'mm-em': value * 0.237106301584,
-                'mm-in': value * 0.39 / 10,
-                'mm-pc': value / 4.42175176,
-                'mm-pt': value / 0.352777777778,
-                'mm-%': value / base * 100 / 2.54 * dpi / 10,
-                'mm-px': value / 2.54 * dpi / 10,
+            'in-cm': value * 2.54,
+            'in-em': value / 0.166044,
+            'in-mm': value * 2.54 * 10,
+            'in-pc': value / 0.166044,
+            'in-pt': value / 0.014842519685,
+            'in-%': value / base * 100 * dpi,
+            'in-px': value * dpi,
 
-                'pc-cm': value * 0.42175176,
-                'pc-em': value,
-                'pc-in': value * 0.166044,
-                'pc-mm': value * 4.42175176,
-                'pc-pt': value / 0.0836458341698,
-                'pc-%': value * 100,
-                'pc-px': value * base,
+            'mm-cm': value / 10,
+            'mm-em': value * 0.237106301584,
+            'mm-in': value * 0.39 / 10,
+            'mm-pc': value / 4.42175176,
+            'mm-pt': value / 0.352777777778,
+            'mm-%': value / base * 100 / 2.54 * dpi / 10,
+            'mm-px': value / 2.54 * dpi / 10,
 
-                'pt-cm': value / 28.3464566929,
-                'pt-em': value / 11.955168,
-                'pt-in': value * 0.014842519685,
-                'pt-mm': value * 0.352777777778,
-                'pt-pc': value * 0.0836458341698,
-                'pt-%': value / (base - 4) * 100,
-                'pt-px': value * 96 / 72,
+            'pc-cm': value * 0.42175176,
+            'pc-em': value,
+            'pc-in': value * 0.166044,
+            'pc-mm': value * 4.42175176,
+            'pc-pt': value / 0.0836458341698,
+            'pc-%': value * 100,
+            'pc-px': value * base,
 
-                '%-cm': value * base / 100 * 2.54 / dpi,
-                '%-em': value / 100,
-                '%-in': value * base / 100 / dpi,
-                '%-mm': value * base / 100 * 2.54 / dpi * 10,
-                '%-pc': value / 100,
-                '%-pt': value * (base - 4) / 100,
-                '%-px': value * base / 100,
+            'pt-cm': value / 28.3464566929,
+            'pt-em': value / 11.955168,
+            'pt-in': value * 0.014842519685,
+            'pt-mm': value * 0.352777777778,
+            'pt-pc': value * 0.0836458341698,
+            'pt-%': value / (base - 4) * 100,
+            'pt-px': value * 96 / 72,
 
-                'px-cm': value * 2.54 / dpi,
-                'px-em': value / base,
-                'px-in': value / dpi,
-                'px-mm': value * 2.54 / dpi * 10,
-                'px-pc': value / base,
-                'px-pt': value * 72 / 96,
-                'px-%': value / base * 100
-            };
+            '%-cm': value * base / 100 * 2.54 / dpi,
+            '%-em': value / 100,
+            '%-in': value * base / 100 / dpi,
+            '%-mm': value * base / 100 * 2.54 / dpi * 10,
+            '%-pc': value / 100,
+            '%-pt': value * (base - 4) / 100,
+            '%-px': value * base / 100,
 
-        result = formulas[units];
+            'px-cm': value * 2.54 / dpi,
+            'px-em': value / base,
+            'px-in': value / dpi,
+            'px-mm': value * 2.54 / dpi * 10,
+            'px-pc': value / base,
+            'px-pt': value * 72 / 96,
+            'px-%': value / base * 100
+        };
+
+        result = FORMULAS[units];
 
         return (isNaN(result) ? 'N/A' : round(result, decimals) + to);
     };
 
     var setupForm = function () {
-        var from = document.querySelector('.from');
-        var selects = document.querySelectorAll('select');
         var settings = document.querySelectorAll('.settings input');
         var fragment = document.createDocumentFragment();
-        var units = ['cm', 'em', 'in', 'mm', 'pc', 'pt', '%', 'px'];
+        var selects = document.querySelectorAll('select');
+        var from = document.querySelector('.from');
+
+        const UNITS = ['cm', 'em', 'in', 'mm', 'pc', 'pt', '%', 'px'];
 
         // Set from value
         from.value = localStorage.getItem(from.name) || '';
 
         // Add the units
-        units.forEach((unit) => {
+        UNITS.forEach((unit) => {
             var option = document.createElement('option');
 
             option.value = unit;
@@ -105,7 +107,7 @@
             var selected;
 
             if (unit) {
-                selected = fragment.querySelector('[value="' + unit + '"]');
+                selected = fragment.querySelector(`[value="${unit}"]`);
                 selected.defaultSelected = true;
             }
 
@@ -118,9 +120,7 @@
 
         // Add settings
         [].forEach.call(settings, (setting) => {
-            var name = setting.name;
-
-            setting.value = localStorage.getItem(name) || setting.value;
+            setting.value = localStorage.getItem(setting.name) || setting.value;
         });
     };
 
@@ -159,4 +159,4 @@
             });
         }, 30000);
     });
-}());
+}(window, document));
