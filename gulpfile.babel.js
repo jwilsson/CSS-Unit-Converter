57a55f6729eeb79800a0aa9037dd0ef2338d29f7
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2016-02-08 21:33:01 +0100

    Better asset revisioning
    Don't use query strings for revisioning, rename files instead.
    Don't include generated index.html in the repo.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,16 @@
 import babel from 'gulp-babel';
 import concat from 'gulp-concat';
 import cssnano from 'gulp-cssnano';
+import del from 'del';
 import eslint from 'gulp-eslint';
+import fs from 'fs';
 import gulp from 'gulp';
 import gulpif from 'gulp-if';
+import handlebars from 'gulp-compile-handlebars';
 import nested from 'postcss-nested'
 import postcss from 'gulp-postcss';
 import rename from 'gulp-rename';
-import rev from 'gulp-rev-append';
+import rev from 'gulp-rev';
 import stylelint from 'stylelint';
 import uglify from 'gulp-uglify';
 import watch from 'gulp-watch';
@@ -15,22 +18,34 @@ import yargs from 'yargs';
 
 const IS_PRODUCTION = !!(yargs.argv.prod); // true if --prod flag is used
 
+gulp.task('clean-css', () => {
+    return del([
+        './assets/*.css'
+    ]);
+});
+
+gulp.task('clean-js', () => {
+    return del([
+        './assets/*.js'
+    ]);
+});
+
 gulp.task('lint-js', () => {
     return gulp.src(['./src/js/**/*.js', '!./src/js/vendor/**/*.js'])
         .pipe(eslint())
         .pipe(eslint.format());
 });
 
-gulp.task('scripts', ['lint-js'], () => {
+gulp.task('scripts', ['clean-js', 'lint-js'], () => {
     return gulp.src('./src/js/**/*.js')
         .pipe(babel())
         .pipe(concat('script.min.js'))
         .pipe(gulpif(IS_PRODUCTION, uglify())) // Only minify when in production
-        .pipe(gulp.dest('./assets/js'));
+        .pipe(gulp.dest('./assets'));
 });
 
-gulp.task('styles', () => {
-    return gulp.src('./src/css/style.css')
+gulp.task('styles', ['clean-css'], () => {
+    return gulp.src('./src/css/*.css')
         .pipe(postcss([
             nested,
             stylelint,
@@ -42,12 +57,26 @@ gulp.task('styles', () => {
             suffix: '.min',
         }))
         .pipe(gulpif(IS_PRODUCTION, cssnano())) // Only minify when in production
-        .pipe(gulp.dest('./assets/css'));
+        .pipe(gulp.dest('./assets'));
 });
 
-gulp.task('rev', function () {
-    return gulp.src('./index.html')
+gulp.task('rev', ['scripts', 'styles'], () => {
+    return gulp.src(['./assets/style.min.css', './assets/script.min.js'])
         .pipe(rev())
+        .pipe(gulp.dest('./assets'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('./assets'))
+});
+
+gulp.task('index', ['rev'], () => {
+    let manifest = JSON.parse(fs.readFileSync('./assets/rev-manifest.json', 'utf8'));
+
+    return gulp.src('./src/index.hbs')
+        .pipe(handlebars(manifest))
+        .pipe(rename({
+            extname: '.html',
+        }))
+
         .pipe(gulp.dest('.'));
 });
 
@@ -61,4 +90,4 @@ gulp.task('watch', ['default'], () => {
     });
 });
 
-gulp.task('default', ['scripts', 'styles', 'rev']);
+gulp.task('default', ['scripts', 'styles', 'index']);
